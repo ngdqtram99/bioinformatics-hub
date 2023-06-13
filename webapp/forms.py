d1fbe88ffd269c3af0bcb8e1e35e84c05b7a63b8
Nikolai <nikolai.klevtsov@mni.thm.de>
2023-06-14 01:34:22 +0200

    implement HorspoolForm

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,1 +1,28 @@
 from django import forms
+import re
+class HorspoolForm(forms.Form):
+    pattern = forms.CharField(label='Muster')
+    sequence = forms.CharField(label='Sequenz', widget=forms.Textarea)
+    direction = forms.ChoiceField(
+        label='Richtung',
+        choices=[('forward', 'Vorwärts'), ('backward', 'Rückwärts')],
+        widget=forms.RadioSelect
+    )
+    def is_valid(self):
+            valid = super().is_valid()
+
+            pattern = self.cleaned_data.get('pattern', '')
+            sequence = self.cleaned_data.get('sequence', '')
+
+            pattern_valid = re.match(r'^[A-Za-z]+$', pattern)
+            sequence_valid = re.match(r'^[A-Za-z]+$', sequence)
+
+            if len(pattern) > len(sequence):
+                 self.add_error('pattern', 'Pattern darf nicht kürzer als Sequenz sein')
+
+            if not pattern_valid:
+                self.add_error('pattern', 'Nur lateinische Buchstaben sind erlaubt.')
+            if not sequence_valid:
+                self.add_error('sequence', 'Nur lateinische Buchstaben sind erlaubt.')
+
+            return valid and pattern_valid and sequence_valid
