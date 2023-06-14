86e75dbab05c7d56572bb97376686f68a9d34fcc
Nikolai <nikolai.klevtsov@mni.thm.de>
2023-06-14 07:20:27 +0200

    implement SimpleSearchForm

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,1 +1,24 @@
 from django import forms
+import re
+class SimpleSearchForm(forms.Form):
+    pattern = forms.CharField(label='Muster')
+    sequence = forms.CharField(label='Sequenz', widget=forms.Textarea)
+
+    def is_valid(self):
+            valid = super().is_valid()
+
+            pattern = self.cleaned_data.get('pattern', '')
+            sequence = self.cleaned_data.get('sequence', '')
+
+            pattern_valid = re.match(r'^[A-Za-z]+$', pattern)
+            sequence_valid = re.match(r'^[A-Za-z]+$', sequence)
+
+            if not pattern_valid:
+                self.add_error('pattern', 'Nur lateinische Buchstaben sind erlaubt.')
+            if not sequence_valid:
+                self.add_error('sequence', 'Nur lateinische Buchstaben sind erlaubt.')
+            if len(pattern) > len(sequence):
+                 pattern_valid = False
+                 self.add_error('pattern', 'Pattern kann nicht länger als Sequenz sein')
+
+            return valid and pattern_valid and sequence_valid
