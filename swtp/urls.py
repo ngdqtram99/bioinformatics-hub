42875949107e3108824eaaf50bb13ed5c7432ead
Nikolai <nikolai.klevtsov@mni.thm.de>
2023-06-14 07:19:57 +0200

    add simple_search to urls

--- swtp/urls.py
+++ swtp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import simple_search_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('simple_search/', simple_search_view, name='simple_search')
 ]
+
