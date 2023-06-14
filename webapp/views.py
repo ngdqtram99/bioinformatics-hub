547ee3bf2e44bb9f51db0aaf550bb374a287b381
Nikolai <nikolai.klevtsov@mni.thm.de>
2023-06-14 02:01:48 +0200

    implement horspool_view

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import HorspoolForm
+from .lib import horspool
+
+def horspool_view(request):
+    form = HorspoolForm()  
+
+    if request.method == 'POST':
+        form = HorspoolForm(request.POST)
+
+        if form.is_valid():
+            pattern = form.cleaned_data['pattern']
+            sequence = form.cleaned_data['sequence']
+            selected_direction = form.cleaned_data['direction']
+            backward = selected_direction == 'backward'
+            results = horspool.get_result(pattern, sequence, backward)
+            print(results)
+            comparsions_count = results['comparsions_result']
+            file_content = f'Es wurden {comparsions_count} Vergleiche gemacht\nEs sind {results_count} Treffer an folgenden Positionen gefunden:\n'+' '.join(str(result) for result in results['results'])
+            results = results['results'][:100]
+
+            context = {
+                'form': form,
+                'results': results,
+                'file_content': file_content,
+                'comparsions_count' : comparsions_count
+            }
+            return render(request, 'horspool.html', context)
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'horspool.html', context)
