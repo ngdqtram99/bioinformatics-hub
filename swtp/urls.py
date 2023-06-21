fbce2240658508b02a5ce06314de39b6a5f6a3e4
jana elsen <jana.elsen@mni.thm.de>
2023-06-21 10:08:15 +0200

    added URL pattern to List

--- swtp/urls.py
+++ swtp/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import simple_search_view
+from webapp.views import dotplot_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('simple_search/', simple_search_view, name='simple_search')
+    path('dotplot/', dotplot_view, name='dotplot')
 ]
 
