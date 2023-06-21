9722eb0a056c31ba68f58c75c9abfb772eee5e36
jana elsen <jana.elsen@mni.thm.de>
2023-06-21 10:11:56 +0200

    Implement view for dotplot algorithm

--- webapp/views.py
+++ webapp/views.py
@@ -1,33 +1,29 @@
 from django.shortcuts import render
+from forms import DotplotForm
+from .lib import dotplot
 
-from .forms import SimpleSearchForm
-from .lib import simple_search
+def dotplot_view(request):
 
-def simple_search_view(request):
-    form = SimpleSearchForm()  
+    form = DotplotForm()
 
     if request.method == 'POST':
-        form = SimpleSearchForm(request.POST)
+        form = DotplotForm(request.POST)
 
         if form.is_valid():
-            pattern = form.cleaned_data['pattern']
-            sequence = form.cleaned_data['sequence']
-            results = simple_search.get_result(pattern, sequence)
-            comparsions_count = results['comparsions_count']
-            results_count = len(results['results'])
-            file_content = f'Es wurden {comparsions_count} Vergleiche gemacht\nEs sind {results_count} Treffer an folgenden Positionen gefunden:\n'+' '.join(str(result) for result in results['results'])
-            results = results['results'][:100]
+            seq1 = form.cleaned_data['sequence1']
+            seq2 = form.cleaned_data['sequence2']
+            result = dotplot.get_result(seq1, seq2)
 
             context = {
                 'form': form,
-                'results': results,             
-                'file_content': file_content, 
-                'resuts_count' : results_count,             
-                'comparsions_count' : comparsions_count
+                'results': result
             }
-            return render(request, 'simple_search.html', context)
+
+            return render(request, 'dotplot.html', context)
 
     context = {
         'form': form,
     }
-    return render(request, 'simple_search.html', context)
+    return render(request, 'dotplot.html', context)
+
+
