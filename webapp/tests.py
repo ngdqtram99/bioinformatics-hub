3bd1ec2bfc00f375650f0f8a176a4e5eb521f34b
tramnguyen <dq.tram.nguyen.99@gmail.com>
2023-06-27 17:29:03 +0200

    add dotplot unittest

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+import lib.dotplot
+import unittest
 
-# Create your tests here.
+class TestDotplot(unittest.TestCase):
+    
+    def test_get_result(self):
+        res1 = [[',','A','T','A','C'],
+                ['A',1,0,1,0],
+                ['C',0,0,0,1],
+                ['C',0,0,0,1],
+                ['A',1,0,1,0]]
+        self.assertEqual(lib.dotplot.get_result('ATAC','ACCA'),res1) 
+
+
+if __name__ == '__main__':
+     unittest.main()
