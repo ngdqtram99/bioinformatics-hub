b40faf36a73b86eb8759dd725321ef039a964941
Nikolai <nikolai.klevtsov@mni.thm.de>
2023-07-09 03:50:22 +0200

    add viterbi to urls

--- swtp/urls.py
+++ swtp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import viterbi_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("viterbi", viterbi_view, name='viterbi')
 ]
