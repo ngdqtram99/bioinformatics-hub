33517a1f88198617cdae298e3b75811a6b884b3e
jana elsen <jana.elsen@mni.thm.de>
2023-07-10 14:40:33 +0200

    wrote tests to test the algorithm

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
+from webapp.lib.simple_search import get_result
 
-# Create your tests here.
+
+# algorithm tests
+class TestAlgorithm(TestCase):
+    def test_comparsions_count(self):
+        # one comparsions
+        # no results
+        result = get_result('T', 'A')
+
+        self.assertEquals(result['comparsions_count'], 1)
+        self.assertEquals(result['results'], [])
+
+        # one result
+        result = get_result('A', 'A')
+
+        self.assertEquals(result['comparsions_count'], 1)
+        self.assertEquals(result['results'], [0])
+
+        # multiple comparsions
+        # no results
+        result = get_result('UA', 'ACGUCAC')
+
+        self.assertEquals(result['comparsions_count'], 7)
+        self.assertEquals(result['results'], [])
+
+        # one result
+        result = get_result('UA', 'ACGUUAC')
+
+        self.assertEquals(result['comparsions_count'], 8)
+        self.assertEquals(result['results'], [4])
+
+        # multiple results
+        result = get_result('UA', 'AUACGUUAUA')
+
+        self.assertEquals(result['comparsions_count'], 13)
+        self.assertEquals(result['results'], [1, 6, 8])
+
+
+    def test_no_result(self):
+        # Sequence to short
+        result = get_result('ACA', 'U')
+
+        self.assertEquals(result['comparsions_count'], 0)
+        self.assertEquals(result['results'], [])
+
+        # Pattern not in Sequence
+        result = get_result('U', 'ACA')
+
+        self.assertEquals(result['comparsions_count'], 3)
+        self.assertEquals(result['results'], [])
+
+
+
+
