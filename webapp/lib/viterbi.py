e7f594165f5210f104070cca83a4f03663d77107
tramnguyen <dq.tram.nguyen.99@gmail.com>
2023-07-18 02:14:45 +0200

    convert coments to german

--- webapp/lib/viterbi.py
+++ webapp/lib/viterbi.py
@@ -1,28 +1,28 @@
-# inputs: seq, emission (set of tuple (tuple of one string and one list)), transition (same as emisison)
-# outputs: matrix, path, state 
+# Inputs: Sequenz, Emissionsmatrix (Set von Tupels (Tulpe: ein String (Zustand) und eine Liste (Wahrscheinlichkeiten))), Übergangmatrix (gleich Type wie Emissionsmatrix)
+# Outputs: Wahrscheinlichkeitsmatrix, Pfad, Zustand Matrix 
 
 from pandas import DataFrame
 import numpy as np
 
-# convert django-defined matrix to dataframe
-## Achtung: Matrix wird transponiert, use DataFrame.transpose to get right matrix
+# Konvertiert django definierte Matrix zu DataFrame
+## Achtung: Matrix wird transponiert, nutzt DataFrame.transpose, um die richtige Matrix zu erreichen
 def to_dataframe(matrix):
-    todict = {} ### we need to have the headline in the matrix too
+    todict = {} ### Kopfzeile (Benennung der Zustände/Symbols sind benötigt)
     for tup in matrix: 
         todict[tup[0]] = tup[1]
         
-    todf = DataFrame(todict, index=todict[' ']) # set headline as index
+    todf = DataFrame(todict, index=todict[' ']) # Setzt die Kopfzeile als Index der DataFrame
     return todf.iloc[:,1:].transpose()
 
-# convert dataframe to a nested list
+# Konvertiert DataFrame zu eine geschachtelte Matrix
 def to_nestedlist(df : DataFrame):
     nestedlist = [[' ']]
-    # add headline
+    # Fügt die Kopfzeile mit den Benennungen ein
     nestedlist[0] += list(df.columns)
-    # add first row
+    # Fügt die erste Spalte mit den Benennungen ein
     for i in df.index: nestedlist.append([i])
 
-    # add values
+    # Fügt die Werte (Wahrscheinlichkeiten/Zustände) ein
     for i in range(1,len(nestedlist)):
         for j in range(1,len(nestedlist[0])):
             nestedlist[i].append(df.iloc[i-1,j-1])
@@ -30,83 +30,89 @@ def to_nestedlist(df : DataFrame):
     #for i in nestedlist: print(i)
     return nestedlist
 
+# Viterbi Algorithmus
 def get_results(seq, transition, emission):
-    # Check conditions
+    # Prüft die Bedingungen
     #---------------------
-    # seq
-    # check: not blank
+    # Sequenz
+    # Prüft, ob Sequenz nicht leer ist
     assert len(seq) > 0, "Die Sequenz muss mindestens einen Buchstabe haben"
     
-    # transition
-    # check 1: 'Start' state exists
-    trans_states = [tup[0] for tup in transition if tup[0] != ' '] # ' ' belongs to headline and is not a state
+    # Übergangsmatrix
+    # Prüft, ob der Start-Zustand vorhanden ist
+    trans_states = [tup[0] for tup in transition if tup[0] != ' '] # ' ' liegt in der Kopfzeile, trotzdem ist kein Zustand
     assert 'Start' in trans_states, "Der Start-Zustand fehlt"
-    # check 2: min. 2 states (except 'Start' state)
+    # Prüft, ob es mindestens 2 Zustände gibt, außer dem Start-Zustand
     assert len(trans_states) >= 2, "Mindestens 2 Zustände eingegeben werden, außer Start-Zustand"
-    
+    # Prüft, ob die Summe der Wahrscheinlichkeiten eines Zustands >= 0 und <= 1 ist
+    for i in range(1,len(transition)):
+        assert sum(transition[i][1]) >= 0 and sum(transition[i][1]) <= 1, "Die Summe der Übergang-Wahrscheinlichkeiten des Zustands {transition[i][0]} ist <= 0 oder >= 1"
 
-    # emission
-    # check 1: no 'Start' state ### "somehow it's nonsence, but maybe form or view has bug :)"
+    # Emissionsmatrix
+    # Prüft, ob es keinen Start-Zustand gibt ### falls Form Bug hat
     em_states = [tup[0] for tup in emission]
     assert 'Start' not in em_states, "Der Start-Zustand sollte nicht da sein"
-    #check 2: all symbols in seq
+    # Prüft, ob alle Symbols in der Matrix auch in der Matrix sind 
     for s in emission[0][1]: assert s in seq, "Die Wahrscheinlichkeit des Symbols {s} fehlt"
+    # Prüft, ob die Summe alle Wahrscheinlichkeiten eines Zustands >= 0 und <= 1 ist
+    for i in range(1,len(emission)):
+        assert sum(emission[i][1]) >= 0 and sum(emission[i][1]) <= 1, "Die Summe der Emission-Wahrscheinlichkeiten des Zustand {emission[i][0]} ist <= 0 oder >= 1"
 
-    # check probability for both matrixes: value in [0,1]
-    for tup in transition[1:]: # except headline 
+    # Prüft jede Wahrscheinlichkeit in beiden Matrizen: Wert in (0,1)
+    for tup in transition[1:]: # außer der Kopfzeile 
         for pro in tup[1]: 
-            assert pro >= 0 and pro <= 1, "Die Wahrscheinlichkeit von {pro} in der Übergangsmatrix ist unpassend"
-    for tup in emission[1:]: # ecxcept headline
+            assert pro > 0 and pro < 1, "Die Wahrscheinlichkeit von {pro} in der Übergangsmatrix ist unpassend"
+    for tup in emission[1:]: # außer der Kopfzeile
         for pro in tup[1]: 
-            assert pro >= 0 and pro <= 1, "Die Wahrscheinlichkeit von {pro} in der Emissionsmatrix ist unpassend"
+            assert pro > 0 and pro < 1, "Die Wahrscheinlichkeit von {pro} in der Emissionsmatrix ist unpassend"
     
-    # Initilization
+    # Initialisierung
     #----------------
-    # seq: add Start symbol
+    # Sequenz: Fügt Start-Symbol ein
     seq = 's' + seq
     
-    # transition and emision: convert to dataFrame 
+    # Matrizen: Konvertiert zu DataFrame 
     trans_df = to_dataframe(transition)
     em_df = to_dataframe(emission)
 
 
-    # probability matrix (dataframe), states matrix and path
+    # Wahrscheinlichkeitsmatrix (DataFrame), Traceback-Pfad und Zustand-Pfad
     pro_df = DataFrame(0.0, index= trans_states, columns= [*seq])
     pro_df.loc['Start','s'] = 1 
-    path = [] # save max probability in each column. 1 presents for the Start state
-    state_path = [] # save state with max probability in each column
+    path = [] ### Gleich wie Wahrscheinlichkeitsmatrix aber speichert Traceback-Pfad
+    state_path = [] # Speichert die Zustände jedes Symbols
 
     state_df = DataFrame(index=['Zustand'],columns=[*seq[1:]])
     print(em_df)
     print(trans_df)
 
-    # Calculating
+    # Berechnen
     # ---------------
     for i_symbol in range(1,len(seq)):
         symbol = pro_df.columns[i_symbol]
-        # first symbol: calculate the probability from Start state
+        # Beim ersten Symbol wird die Wahrscheinlichkeit von Start-Zustand gerechnet
         if i_symbol == 1:
             for state in pro_df.index[1:]:
                 pro_df.loc[state][i_symbol] = pro_df.loc['Start']['s'] * trans_df.loc['Start'][state] * em_df.loc[state][symbol]
             #print(pro_df)
 
-        # other symbol: calculate the probability from pre-symbol's probability
+        # Beim übrige Symbole werden ihre Wahrscheinlichkeit von die maximalen vorangegangenen gerechnet werden
         else:    
             for state in pro_df.index[1:]:
                 pro_df.loc[state][i_symbol] = path[-1] * trans_df.loc[state_path[-1]][state] * em_df.loc[state][symbol]
             #print(pro_df)
         
-        # add max and state of max of the present symbol in the path and state_path
+        ### add max and state of max of the present symbol in the path and state_path
         path.append(pro_df.iloc[:,i_symbol].max())
         #print(path)
         state_path.append(pro_df.iloc[:,i_symbol].idxmax())
         #print(state_path)
         
-    assert len(state_path) == len(seq[1:]) # check, if there are enough states for each symbol in the seq (just for sure)
+    assert len(state_path) == len(seq[1:]) # Prüft, ob die Anzahl der Zustände die Länge der Sequenz entspricht
     # add state in state_df
     state_df.loc['Zustand'] = state_path
 
-    # create log probability matrix and log path following the probability matrix and path
+    # Erzeugt die log-Wahrscheinlichkeitsmatrix ### keine log-Pfad benötigt??
     logpro_df = np.log(pro_df)
     logpath = [np.log(p) for p in path]
     
@@ -123,7 +129,7 @@ def get_results(seq, transition, emission):
             'path_matrix': to_nestedlist(state_df)}
     
 
-# Example
+# Beispiel
 trans = [(' ',['+','-']),
       ('Start',[0.5,0.5]),
       ('+',[0.4,0.6]),
