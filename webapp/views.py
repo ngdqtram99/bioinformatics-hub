f06d7124510f5cbf0236ba3fd3703b97718e8dcc
Nikolai <nikolai.klevtsov@mni.thm.de>
2023-07-22 17:17:09 +0200

    correct a typo in simple_search view and template

--- webapp/views.py
+++ webapp/views.py
@@ -47,9 +47,9 @@ def simple_search_view(request):
             pattern = form.cleaned_data['pattern']
             sequence = form.cleaned_data['sequence']
             results = simple_search.get_result(pattern, sequence)
-            comparsions_count = results['comparsions_count']
+            comparisons_count = results['comparisons_count']
             results_count = len(results['results'])
-            file_content = f'Es wurden {comparsions_count} Vergleiche gemacht\nEs sind {results_count} Treffer an folgenden Positionen gefunden:\n'+' '.join(str(result) for result in results['results'])
+            file_content = f'Es wurden {comparisons_count} Vergleiche gemacht\nEs sind {results_count} Treffer an folgenden Positionen gefunden:\n'+' '.join(str(result) for result in results['results'])
             results = results['results'][:100]
 
             context = {
@@ -57,7 +57,7 @@ def simple_search_view(request):
                 'results': results,             
                 'file_content': file_content, 
                 'results_count' : results_count,             
-                'comparsions_count' : comparsions_count
+                'comparisons_count' : comparisons_count
             }
             return render(request, 'simple_search.html', context)
 
