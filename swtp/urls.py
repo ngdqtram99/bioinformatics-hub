210fe74144d1b7d2dba655b92f53f2acdece0cf7
tntha <tnthao26899@gmail.com>
2023-07-24 19:11:14 +0200

    Add paths of different templates

--- swtp/urls.py
+++ swtp/urls.py
@@ -25,6 +25,14 @@ urlpatterns = [
     path('dotplot/', dotplot_view, name='dotplot'),
     path('simple_search/', simple_search_view, name='simple_search'),
     path('overlap/', overlap_view, name='overlap'),
-    path('needleman_wunsch/', needleman_wunsch_view, name='needleman_wunsch')
+    path('needleman_wunsch/', needleman_wunsch_view, name='needleman_wunsch'),
+    path('smith_waterman/', smith_waterman_view, name='smith_waterman'),
+    path('upgma/', upgma_view, name='upgma'),
+    path('neighbour_joining/', neighbour_joining_view, name='neighbour_joining'),
+    path('suffix_tree/', suffix_tree_view, name='suffix_tree'),
+    path('suffix_trie/', suffix_trie_view, name='suffix_trie'),
+    path('suffix_array/', suffix_array_view, name='suffix_array'),
+    path('glocal_alignment/', glocal_alignment_view, name='glocal_alignment'),
 ]
+
 
