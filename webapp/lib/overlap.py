dba8b2faaee1f2b6171024ca9fbc66209aa4bdb4
tramnguyen <dq.tram.nguyen.99@gmail.com>
2023-07-25 20:27:58 +0200

    use nested lists for matrix and traceback instead of Dataframe

--- webapp/lib/overlap.py
+++ webapp/lib/overlap.py
@@ -1,17 +1,3 @@
-from pandas import DataFrame
-
-# Konvertiert zur geschachtelte Matrix
-def to_matrix(dataframe : DataFrame):
-    matrix = [[' ']]
-    matrix[0] += list(dataframe.columns)
-
-    for i in dataframe.index: matrix.append([i])
-
-    for i in range(1,len(matrix)):
-        matrix[i].extend(list(dataframe.iloc[i-1]))
-
-    return matrix
-
 # Traceback-Wert
 def traceback_value(dia,ver,hor, similarity : bool):
     
@@ -21,52 +7,48 @@ def traceback_value(dia,ver,hor, similarity : bool):
     else: return "hor"
 
 # Gibt die Alignment zurück
-def get_alignment(traceback : DataFrame, position: int):
+def get_alignment(sequence1 : str, sequence2 : str, traceback : list, position: int):
 
     s1_alg = ""
     s2_alg = ""
     
-    i = len(traceback.index) - 1 
+    i = len(traceback) - 1
     j  = position
-
-    seq1 = traceback.index
-    seq2 = traceback.columns
 
     # Alignment erstellen
     # Addiert Substring der Sequenz 2, die nicht zum Überlapp-Teil gehört
-    for c in range(j+1, len(traceback.columns)):
-        s2_alg += seq2[c]
+    for pos in range(j+1, len(sequence2)):
+        s2_alg += sequence2[pos]
 
     # Addiert die Überlapp-Substring
     while i > 0 and j > 0:
-        if traceback.iloc[i][j] == "dia":
-            s1_alg = seq1[i] + s1_alg
-            s2_alg = seq2[j] + s2_alg
+        if traceback[i][j] == "dia":
+            s1_alg = sequence1[i] + s1_alg
+            s2_alg = sequence2[j] + s2_alg
             i -= 1 ; j -= 1
         elif traceback[i][j] == "ver":
-            s1_alg = seq1[i] + s1_alg
+            s1_alg = sequence1[i] + s1_alg
             s2_alg = "-" + s2_alg
             i -= 1
         elif traceback[i][j] == "hor":
             s1_alg = "-" + s1_alg
-            s2_alg = seq2[j] + s2_alg
+            s2_alg = sequence2[j] + s2_alg
             j -= 1
-    
+
     # Addiert Substring der Sequenz 1, die nicht zum Überlapp-Teil gehört
     while i > 0:
-        s1_alg = seq1[i] + s1_alg
+        s1_alg = sequence1[i] + s1_alg
         s2_alg = '-' + s2_alg
         i -= 1
-    
+
     return [[*s1_alg],[*s2_alg]]
 
 # Überlapp-Algorithmus
-def get_result(sequence1: str, sequence2: str, similarity = True, match = 1, mismatch = -1, gap_penalty = -1):
+def get_result(sequence1: str, sequence2: str, match : int, mismatch : int, gap_penalty : int, similarity: bool):
     seq1,seq2 = '-' + sequence1, '-' + sequence2
 
-    traceback = DataFrame(None, index=[*seq1], columns=[*seq2])
-    matrix = DataFrame(0, index=[*seq1], columns=[*seq2])    
-
+    traceback = [[None for j in range(len(seq2))] for i in range(len(seq1))]
+    matrix = [[0 for j in range(len(seq2))] for i in range(len(seq1))]
     # Prüft die Vorbedingungen von Match, Mismatch und Gap-Penalty
     if similarity:
         assert gap_penalty < 0, 'Gap-Penalty sollte kleiner 0 sein'
@@ -78,41 +60,42 @@ def get_result(sequence1: str, sequence2: str, similarity = True, match = 1, mis
         assert match < 0, 'Match sollte kleiner als 0 sein'
 
     #Initialisierung
-    for i in range(1,len(matrix.columns)):
-        matrix.loc['-'][i] = matrix.loc['-'][i-1] + gap_penalty
-    
+    for i in range(1,len(seq2)):
+        matrix[0][i] = matrix[0][i-1] + gap_penalty
+
     # Berechnet Werte in der Matrix
-    for i in range(1,len(matrix.index)):
-        for j in range(1,len(matrix.columns)):
-            s = match if matrix.index[i] == matrix.columns[j] else mismatch
-            ver = matrix.iloc[i-1][j] + gap_penalty
-            hor = matrix.iloc[i][j-1] + gap_penalty
-            dia = matrix.iloc[i-1][j-1] + s
+    for i in range(1,len(seq1)):
+        for j in range(1,len(seq2)):
+            s = match if seq1[i] == seq2[j] else mismatch
             
-            matrix.iloc[i][j] = max(dia,ver,hor) if similarity else min(dia,ver,hor)
+            ver = matrix[i-1][j] + gap_penalty
+            hor = matrix[i][j-1] + gap_penalty
+            dia = matrix[i-1][j-1] + s
             
-            traceback.iloc[i][j] = traceback_value(dia,ver,hor,similarity)
+            matrix[i][j] = max(dia,ver,hor) if similarity else min(dia,ver,hor)
+            
+            traceback[i][j] = traceback_value(dia,ver,hor,similarity)
     
-    # Bestes Score
-    max_value = max(matrix.iloc[len(sequence1)]) 
-    # Positionen des besten Score. Viele Positionen die den gleichen Score besitzen
-    for i in range(len(matrix.columns)):
-        if matrix.iloc[len(sequence1)][i] == max_value: 
+    # Bestes Score der letzten Zeile der Scorematrix
+    max_value = max(matrix[-1]) 
+    # Positionen des besten Score. Weil es möglich ist, viele Positionen den selben Score besitzen, behält die Methode nur die im Matrix ganz rechten Position 
+    for i in range(len(seq2)):
+        if matrix[-1][i] == max_value: 
             pos = i
 
     # Alignment entspricht jedes besten Score
-    alignments = get_alignment(traceback, pos)
+    alignments = get_alignment(seq1, seq2, traceback, pos)
 
-    '''	
+    '''
     print(matrix)
     print(traceback)
     for i in alignments: print(i)
     '''
 
-    return {'matrix': to_matrix(matrix),
+    return {'matrix': matrix,
             'alignments': alignments,
             'score': max_value}
 
 # Beispiel
-res = get_result('AAAN','ANNA')
+res = get_result('AAAN','ANNA', 2, -1 ,-2, True)
 #for i in res: print(res[i])
