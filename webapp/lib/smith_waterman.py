f3c1e4b8f92426c2c3b57b5b3b14027ef2d7391d
tramnguyen <dq.tram.nguyen.99@gmail.com>
2023-08-09 00:21:26 +0200

    correct get_alignment, optimize code to get more hits

--- webapp/lib/smith_waterman.py
+++ webapp/lib/smith_waterman.py
@@ -1,5 +1,5 @@
-from traceback import _get_paths, _modify_path
-
+#from traceback import _get_paths, _modify_path
+from webapp.lib.traceback import _get_paths, _modify_path
 
 # Gibt die Richtung/Vorgänger zurück
 def traceback_value(dia, ver, hor, none):
@@ -24,14 +24,24 @@ def traceback_value(dia, ver, hor, none):
 
 
 # Gibt die Alignment zurück
-def get_alignment(seq1: str, seq2: str, path: list):
+def get_alignment(seq1: str, seq2: str, path: list, start_pos: list):
     s1_alg = ""  # Alignment von Sequenz 1
     alg = ""  # Wenn die Buchstaben identisch an der selben Stelle zweier Sequenzen sind, ergibt '|', sonst ' ' (leer-Zeichen)
     s2_alg = ""  # Alignment von Sequenz 2
-
+    
     # Start von ganz unten rechts (globales Alignment)
-    i = len(seq1) - 1
-    j = len(seq2) - 1
+    i = start_pos[0]
+    j = start_pos[1]
+    
+    for x in range(len(seq1)-1,i,-1):
+        s1_alg = seq1[x] + s1_alg
+        s2_alg = '_' + s2_alg
+        alg += '_'
+
+    for y in range(len(seq2)-1,j,-1):
+        s2_alg = seq2[y] + s2_alg
+        s1_alg += '_'
+        alg += '_'
 
     # Erstellt Alignment
     for p in path:
@@ -42,17 +52,25 @@ def get_alignment(seq1: str, seq2: str, path: list):
             i -= 1;
             j -= 1
 
-        elif p == "vert":
-            s1_alg = seq1[i] + s1_alg
-            s2_alg = "-" + s2_alg
-            alg = ' ' + alg
-            i -= 1
+    while i > 0 and j > 0:
+        s1_alg = seq1[i] + s1_alg
+        s2_alg = seq2[j] + s2_alg
+        alg = '_' + alg
+        i -= 1 ; j -= 1
 
-        elif p == "hor":
-            s1_alg = "-" + s1_alg
-            s2_alg = seq2[j] + s2_alg
-            alg = ' ' + alg
-            j -= 1
+    while i > 0:
+        s1_alg = seq1[i] + s1_alg
+        s2_alg = '_' + s2_alg
+        alg = '_' + alg
+        i -= 1
+
+    while j > 0:
+        s1_alg = '_' + s1_alg
+        s2_alg = seq2[j] + s2_alg
+        alg = '_' + alg
+        j -= 1
+    
+    print('\nalignment\n',s1_alg,'\n',alg,'\n',s2_alg,'\n')
 
     return [s1_alg, alg, s2_alg]
 
@@ -76,8 +94,8 @@ def get_result(sequence1, sequence2, match, missmatch, gap_penalty):
         for j in range(1, len(seq2)):
 
             diag = matrix[i-1][j-1] + is_match(seq1[i], seq2[j], match, missmatch)
-            vert = matrix[i][j-1]-gap_penalty
-            hor = matrix[i-1][j]-gap_penalty
+            vert = matrix[i][j-1]+gap_penalty
+            hor = matrix[i-1][j]+gap_penalty
             none = 0
             maxScore = max(diag, vert, hor, none)
             matrix[i][j] = maxScore
@@ -89,20 +107,45 @@ def get_result(sequence1, sequence2, match, missmatch, gap_penalty):
                 max_i = i
                 max_j = j
 
+    #for i in matrix: print(i)
+    #for j in traceback: print(j)
 
-    start_pos = [max_i, max_j]
+    start_pos = []
+    for i in range(len(seq1)):
+        for j in range(len(seq2)):
+            if matrix[i][j] == k: start_pos.append([i,j])
+    print(start_pos)
 
-    unmodified_paths = _get_paths(traceback, start_pos)
+    unmodified_paths = [_get_paths(traceback, pos) for pos in start_pos]
 
-    alignments = [{'alignment': get_alignment(seq1, seq2, path), 'path': _modify_path(path, start_pos)} for path in
-                  unmodified_paths]
+
+    for i in range(len(unmodified_paths)):
+        sm_paths = [] # Smith-Watermann-Path: beinhalt nur Paths, die nur diagonale Richtungen enthälten
+        for path in unmodified_paths[i]:
+            if 'vert' not in path and 'hor' not in path:
+                sm_paths.append(path)
+        unmodified_paths[i] = sm_paths
+    
+    #print('unmodified_paths', unmodified_paths)
+
+    #start_pos = [max_i, max_j]
+
+    start_pos = []
+    for i in range(len(seq1)):
+        for j in range(len(seq2)):
+            if matrix[i][j] == k: start_pos.append([i,j])
+
+    alignments = []
+    for p in range(len(start_pos)):
+        alignments.extend([{'alignment': get_alignment(seq1, seq2, path, start_pos[p]), 'path': _modify_path(path, start_pos[p])} for path in unmodified_paths[p]])
 
     score = k
-
+    print('score',score)
     return {'matrix': matrix,
             'alignments': alignments,
             'score': score}
 
-
+res = get_result('fcggtcggtca','ggtc',2,-1,-2)
+#print(res['alignments'])
 
 
