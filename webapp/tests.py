ada9aa128da6fb5d948d9b14854c66e598105aaa
jana elsen <jana.elsen@mni.thm.de>
2023-08-29 15:07:57 +0200

    Added tests for glocal_alignment.py

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,46 +1,46 @@
 from django.test import TestCase
-from .lib import dotplot
-from .lib import simple_search
+from .lib import dotplot, simple_search, horspool, glocal_alignment
 import unittest
 from .forms import DotplotForm
-from .lib import horspool
+
 
 class TestDotplot(TestCase):
-    
+
     def test_get_result(self):
         # Fall 1: Sequenzen mit der gleichen Länge
-        res1 = [[' ','A','T','A','C'],
-                ['A',1,0,1,0],
-                ['C',0,0,0,1],
-                ['C',0,0,0,1],
-                ['A',1,0,1,0]]
-        self.assertEqual(dotplot.get_result('ATAC','ACCA'),res1) 
+        res1 = [[' ', 'A', 'T', 'A', 'C'],
+                ['A', 1, 0, 1, 0],
+                ['C', 0, 0, 0, 1],
+                ['C', 0, 0, 0, 1],
+                ['A', 1, 0, 1, 0]]
+        self.assertEqual(dotplot.get_result('ATAC', 'ACCA'), res1)
 
         # Fall 2: Sequenzen mit unterschiedlicher Längen
-        res2 = [[' ','A','T','A','C'],
-                ['A',1,0,1,0],
-                ['C',0,0,0,1],
-                ['C',0,0,0,1]]
-        self.assertEqual(dotplot.get_result('ATAC','ACC'),res2)
+        res2 = [[' ', 'A', 'T', 'A', 'C'],
+                ['A', 1, 0, 1, 0],
+                ['C', 0, 0, 0, 1],
+                ['C', 0, 0, 0, 1]]
+        self.assertEqual(dotplot.get_result('ATAC', 'ACC'), res2)
 
         # Fall 3: Eine Sequenz fehlt
-        res3 = [[' ','A','A','A','C']]
-        self.assertEqual(dotplot.get_result('AAAC',''),res3) 
+        res3 = [[' ', 'A', 'A', 'A', 'C']]
+        self.assertEqual(dotplot.get_result('AAAC', ''), res3)
 
         res4 = [[' '],
                 ['C'],
                 ['C'],
                 ['G']]
-        self.assertEqual(dotplot.get_result('','CCG'),res4)
+        self.assertEqual(dotplot.get_result('', 'CCG'), res4)
 
         # Fall 4: Keine Sequenzen
         res5 = [[' ']]
-        self.assertEqual(dotplot.get_result('',''),res5)
+        self.assertEqual(dotplot.get_result('', ''), res5)
+
 
 class TestDotplotForm(TestCase):
-    
+
     def test_latin_letters(self):
-        form = DotplotForm(data = {'sequence1':'ATR-R', 'sequence2':'XX*WQ'})
+        form = DotplotForm(data={'sequence1': 'ATR-R', 'sequence2': 'XX*WQ'})
         form.is_valid()
         self.assertEqual(form.errors['sequence1'], ['Nur lateinische Buchstaben sind erlaubt.'])
         self.assertEqual(form.errors['sequence2'], ['Nur lateinische Buchstaben sind erlaubt.'])
@@ -80,7 +80,6 @@ class TestSimpleSearch(unittest.TestCase):
 
         self.assertEquals(result['comparisons_count'], 13)
         self.assertEquals(result['results'], [1, 6, 8])
-
 
     def test_no_result(self):
         # Sequence to short
@@ -185,4 +184,102 @@ class TestHorspool(TestCase):
 
         self.assertEquals(result['comparisons_count'], 6)
         self.assertEquals(result['results'], [0, 3])
+
+
+# glocal_alignment tests
+class TestGlocalAlignment(TestCase):
+
+    # testet Form
+
+
+    # testet ob das richtige Alignment zurück kommt
+    def test_get_alignment(self):
+        # alignment mit Gap in der Sequenz
+        alignment = glocal_alignment.get_alignment('-GCTAGC', '-GCT', ['hor', 'diag', 'diag'], [2, 3])
+        self.assertEquals(alignment, ['GC-TAGC', '||     ', 'GCT    '])
+
+        # alignment mit Gap im Pattern
+        alignment = glocal_alignment.get_alignment('-GCTAGC', '-GT', ['diag', 'vert', 'diag'], [3, 2])
+        self.assertEquals(alignment, ['GCTAGC', '| |   ', 'G-T   '])
+
+        # alignment ohne Gap
+        alignment = glocal_alignment.get_alignment('-GCTAGC', '-GCT', ['diag', 'diag', 'diag'], [3, 3])
+        self.assertEquals(alignment, ['GCTAGC', '|||   ', 'GCT   '])
+
+    # testet ob die richtige traceback value zurück gegeben wird
+    def test_traceback_value(self):
+        # Similarity ist auf False, der kleinste Wert wird gewählt - hier vert
+        traceback_value = glocal_alignment.traceback_value(2, 1, 3, False)
+        self.assertEquals(traceback_value, 'vert')
+
+        # Similarity ist auf True, der größte Wert wird gewählt - hier diag
+        traceback_value = glocal_alignment.traceback_value(6, 2, 3, True)
+        self.assertEquals(traceback_value, 'diag')
+
+        # mehrere Werte sind gleich
+        traceback_value = glocal_alignment.traceback_value(3, 3, 5, False)
+        self.assertEquals(traceback_value, ['diag', 'vert'])
+
+        traceback_value = glocal_alignment.traceback_value(4, 5, 4, False)
+        self.assertEquals(traceback_value, ['diag', 'hor'])
+
+        traceback_value = glocal_alignment.traceback_value(5, 3, 3, False)
+        self.assertEquals(traceback_value, ['vert', 'hor'])
+
+    # testet den Schwellwert
+    def test_threshold(self):
+        # Nur Alignments mit einem Score 2 oder weniger werden ausgegeben
+        result = glocal_alignment.get_result('PONY', 'PONY', 0, 1, 1, False, 2)
+
+        self.assertFalse({3, 4, 5}.issubset(result['score']))
+        self.assertEquals(len(result['score']), 3)
+
+        # Der Schwellwert ist bei 8, also wird kein Score heraus gefiltert
+        result = glocal_alignment.get_result('PONY', 'PONY', 0, 1, 1, False, 8)
+
+        self.assertTrue({0, 1, 2, 3, 4}.issubset(result['score']))
+        self.assertEquals(len(result['score']), 5)
+
+        # Der Schwellwert liegt so niedrig das kein Alignment diesen Score hat und nichts wird ausgegeben
+        result = glocal_alignment.get_result('PONY', 'PONY', 0, 1, 1, False, -1)
+
+        self.assertEquals(len(result['score']), 0)
+
+    # testet die Funktion similarity
+    def test_similarity_matrix(self):
+        # similarity ist auf False gesetzt, somit wird immer der minimalste Wert gewählt
+        result = glocal_alignment.get_result('PXPOYXPONY', 'PONY', 0, 1, 1, False, 1)
+
+        distance_matrix= [[0, 1, 2, 3, 4],
+                         [0, 0, 1, 2, 3],
+                         [0, 1, 1, 2, 3],
+                         [0, 0, 1, 2, 3],
+                         [0, 1, 0, 1, 2],
+                         [0, 1, 1, 1, 1],
+                         [0, 1, 2, 2, 2],
+                         [0, 0, 1, 2, 3],
+                         [0, 1, 0, 1, 2],
+                         [0, 1, 1, 0, 1],
+                         [0, 1, 2, 1, 0]]
+
+        self.assertEquals(distance_matrix, result['matrix'])
+
+        # similarity wird auf True gesetzt, somit wird immer der maximale Wert gewählt
+        result = glocal_alignment.get_result('PXPOYXPONY', 'PONY', 0, 1, 1, True, 1)
+
+        similarity_matrix = [[0, 1, 2, 3, 4],
+                             [0, 2, 3, 4, 5],
+                             [0, 3, 4, 5, 6],
+                             [0, 4, 5, 6, 7],
+                             [0, 5, 6, 7, 8],
+                             [0, 6, 7, 8, 9],
+                             [0, 7, 8, 9, 10],
+                             [0, 8, 9, 10, 11],
+                             [0, 9, 10, 11, 12],
+                             [0, 10, 11, 12, 13],
+                             [0, 11, 12, 13, 14]]
+
+        self.assertEquals(similarity_matrix, result['matrix'])
+
+
 
