fff0f2a3a969c0dba767ce95b499e19e0ea328cf
tntha <tnthao26899@gmail.com>
2023-08-31 19:18:36 +0200

    Fixed the input fields

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 import re
 class HorspoolForm(forms.Form):
-    pattern = forms.CharField(label='Muster', strip=False, widget=forms.TextInput(attrs={"class": "max-width-input"}))
-    sequence = forms.CharField(label='Sequenz', widget=forms.Textarea(attrs={"class": "max-width-input"}), strip=False)
+    pattern = forms.CharField(label='Muster', widget=forms.TextInput(attrs={"size": "167"}))
+    sequence = forms.CharField(label='Sequenz', widget=forms.TextInput(attrs={"size": "167"}))
     direction = forms.ChoiceField(
         label='Richtung',
         choices=[('forward', 'Vorwärts'), ('backward', 'Rückwärts')],
@@ -28,7 +28,7 @@ class HorspoolForm(forms.Form):
 
         if len(pattern) > len(sequence):
                 valid = False
-                self.add_error('pattern', 'Pattern kann nicht länger als Sequenz sein')
+                self.add_error('pattern', 'Pattern darf nicht länger als Sequenz sein')
 
         return valid 
 
@@ -54,8 +54,8 @@ class DotplotForm(forms.Form):
         return valid and sequence1_valid and sequence2_valid
     
 class SimpleSearchForm(forms.Form):
-    pattern = forms.CharField(label='Muster', strip=False,widget=forms.TextInput(attrs={"class": "max-width-input"}))
-    sequence = forms.CharField(label='Sequenz', widget=forms.Textarea(attrs={"class": "max-width-input"}), strip=False)
+    pattern = forms.CharField(label='Muster', strip=False,widget=forms.TextInput(attrs={"size": "167"}))
+    sequence = forms.CharField(label='Sequenz', widget=forms.TextInput(attrs={"size": "167"}))
     ignore_case_choice = forms.BooleanField(label='Groß-/Kleinschreibung ignorieren', required=False, initial=False, widget=forms.CheckboxInput())
     
     def clean(self):
@@ -76,13 +76,13 @@ class SimpleSearchForm(forms.Form):
 
         if len(pattern) > len(sequence):
                 valid = False
-                self.add_error('pattern', 'Pattern kann nicht länger als Sequenz sein')
+                self.add_error('pattern', 'Pattern darf nicht länger als Sequenz sein')
 
         return valid 
     
 class OverlapForm(forms.Form):
-    sequence1 = forms.CharField(label='Sequenz 1', widget=forms.TextInput(attrs={"class": "max-width-input"}))
-    sequence2 = forms.CharField(label='Sequenz 2', widget=forms.TextInput(attrs={"class": "max-width-input"}))
+    sequence1 = forms.CharField(label='Sequenz 1', widget=forms.TextInput(attrs={"size": "167"}))
+    sequence2 = forms.CharField(label='Sequenz 2', widget=forms.TextInput(attrs={"size": "167"}))
     match = forms.FloatField(label='Match', min_value=-1000, max_value=1000, initial=-1, widget=forms.TextInput(attrs={'size': '10'}))
     mismatch = forms.FloatField(label='Mismatch', min_value=-1000, max_value=1000, initial=1, widget=forms.TextInput(attrs={'size': '10'}))
     gap_penalty = forms.FloatField(label='Gap-Score', min_value=-1000, max_value=1000, initial=1, widget=forms.TextInput(attrs={'size': '10'}))
@@ -131,8 +131,8 @@ class SmithWatermanForm(forms.Form):
             return valid and sequence1_valid and sequence2_valid
     
 class NeedlemanWunschForm(forms.Form):
-    sequence1 = forms.CharField(label='Sequenz 1', widget=forms.TextInput(attrs={"class": "max-width-input"}))
-    sequence2 = forms.CharField(label='Sequenz 2', widget=forms.TextInput(attrs={"class": "max-width-input"}))
+    sequence1 = forms.CharField(label='Sequenz 1', widget=forms.TextInput(attrs={"size": "167"}))
+    sequence2 = forms.CharField(label='Sequenz 2', widget=forms.TextInput(attrs={"size": "167"}))
     match = forms.FloatField(label='Match', min_value=-1000, max_value=1000, initial=0, widget=forms.TextInput(attrs={'size': '10'}))
     mismatch = forms.FloatField(label='Mismatch', min_value=-1000, max_value=1000, initial=1, widget=forms.TextInput(attrs={'size': '10'}))
     gap_penalty = forms.FloatField(label='Gap-Score', min_value=-1000, max_value=1000, initial=1, widget=forms.TextInput(attrs={'size': '10'}))
@@ -157,8 +157,8 @@ class NeedlemanWunschForm(forms.Form):
             return valid and sequence1_valid and sequence2_valid
     
 class GlocalAlignmentForm(forms.Form):
-    sequence1 = forms.CharField(label='Sequenz 1', widget=forms.TextInput(attrs={"class": "max-width-input"}))
-    sequence2 = forms.CharField(label='Sequenz 2', widget=forms.TextInput(attrs={"class": "max-width-input"}))
+    sequence1 = forms.CharField(label='Sequenz 1', widget=forms.TextInput(attrs={"size": "167"}))
+    sequence2 = forms.CharField(label='Sequenz 2', widget=forms.TextInput(attrs={"size": "167"}))
     match = forms.FloatField(label='Match', min_value=-1000, max_value=1000, initial=-1, widget=forms.TextInput(attrs={'size': '10'}))
     mismatch = forms.FloatField(label='Mismatch', min_value=-1000, max_value=1000, initial=1, widget=forms.TextInput(attrs={'size': '10'}))
     gap_penalty = forms.FloatField(label='Gap-Score', min_value=-1000, max_value=1000, initial=1,widget=forms.TextInput(attrs={'size': '10'}))
@@ -184,8 +184,8 @@ class GlocalAlignmentForm(forms.Form):
             return valid and sequence1_valid and sequence2_valid    
 
 class ViterbiForm(forms.Form):
-    sequence = forms.CharField(label='Sequenz')
-    states = forms.CharField(label='Zustände')
+    sequence = forms.CharField(label='Sequenz', widget=forms.TextInput(attrs={"size": "167"}))
+    states = forms.CharField(label='Zustände', widget=forms.TextInput(attrs={"size": "167"}))
 
     def clean(self):
         cleaned_data = super().clean()
@@ -297,14 +297,14 @@ class SuffixArrayForm(forms.Form):
 
         if len(pattern) > len(sequence):
                 valid = False
-                self.add_error('pattern', 'Pattern kann nicht länger als Sequenz sein')
+                self.add_error('pattern', 'Pattern darf nicht länger als Sequenz sein')
 
         return valid 
     
 class SuffixTreeTrieForm(forms.Form):
-    sequence = forms.CharField(label='Text', widget=forms.TextInput(attrs={"class": "max-width-input"}), strip=False)
-    pattern = forms.CharField(label='Muster', required=False, strip=False, widget=forms.TextInput(attrs={"class": "max-width-input"}))
-    endchar = forms.CharField(label='Endzeichen', max_length=1, min_length=1, initial='$', widget=forms.TextInput(attrs={"class": "max-width-input"}))
+    sequence = forms.CharField(label='Text', widget=forms.TextInput(attrs={"size": "167"}))
+    pattern = forms.CharField(label='Muster', required=False, strip=False, widget=forms.TextInput(attrs={"size": "167"}))
+    endchar = forms.CharField(label='Endzeichen', max_length=1, min_length=1, initial='$', widget=forms.TextInput(attrs={"size": "167"}))
 
     def is_valid(self):
         valid = super().is_valid()
@@ -314,6 +314,6 @@ class SuffixTreeTrieForm(forms.Form):
 
         if len(pattern) > len(sequence):
                 valid = False
-                self.add_error('pattern', 'Pattern kann nicht länger als Sequenz sein')
+                self.add_error('pattern', 'Pattern darf nicht länger als Sequenz sein')
 
         return valid 
