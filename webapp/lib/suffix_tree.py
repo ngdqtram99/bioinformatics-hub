0482ee5dd16fdff74e4cb8e8bd875abe722c845d
tramnguyen <dq.tram.nguyen.99@gmail.com>
2023-08-31 15:21:57 +0200

    correct find_hits, add more comments, change output of get_results, update some variables

--- webapp/lib/suffix_tree.py
+++ webapp/lib/suffix_tree.py
@@ -1,3 +1,4 @@
+# Node(childeren, positions) im Suffix-Baum
 class Node:
     def __init__(self):
         self.children = {} # die Schlüssels sind die Charaktere des nächsten Nodes, während die Werte ihres Node sind
@@ -6,10 +7,11 @@ class Node:
     def is_end_node(self):
         return len(self.children) == 0
     
-# Baut ein Suffix Trie aus einem Text auf
+# Baut ein Suffix Trie aus einem Text auf (ähnlich Code wie Aufbau des Suffix-Trie aber mit Node statt TrieNode)
 def build_suffix_trie(text: str, endchar: str):
-    assert len(endchar) == 1
-    text += endchar
+    assert len(endchar) == 1 # Prüft, das Endcharakter nur 1 Buchstabe enthält
+    
+    text += endchar # Addiert Endcharakter am Ende des Textes
     root = Node()
 
     for i in range(len(text)):
@@ -23,42 +25,51 @@ def build_suffix_trie(text: str, endchar: str):
 
     return root
 
+# Baut einen Suffix-Baum von einem gabauten Suffix-Trie auf
 def build_tree(trie: Node):
+
+    # Fügt die Noden, die nur ein Kind hat, zusammen
     def merge_node(node: Node):
         #print('node',node.children.keys(), node.positions)
         #print('--------------------------')
         
+        # Endbedingung, wenn es das Endnode trifft
         if node.is_end_node() : return node
 
+        # Kopiert das Dictionary von node.children, weil node.children in der for-Schleife geändert wird und danach einen Fehler trifft
         current_node_children = node.children.copy()
-        for i, (name_child, child) in enumerate(current_node_children.items()):
+        for child_name, child in current_node_children.items():
             
+            # Fügt Kind- und Enkel-Node zusammen wenn das Kind-Node nur ein Enkel-Node enthält 
             if len(child.children) == 1:
-                grandchild = child.children.popitem()
-                new_name = name_child + grandchild[0] # + grandchild_name
-                new_child = grandchild[1] # grandchild Node
+                grandchild = child.children.popitem() # Variable: Namen und des Nodes des Enkel-Nodes
+                new_name = child_name + grandchild[0] # Neuer Name von Kind-Node: child_name + grandchild_name
+                new_child = grandchild[1] # Enkel-Node
                 
                 #print('b4 node.children',node.children.keys(),node.positions)
 
-                node.children.pop(name_child)
+                # Löscht das alte Kind-Node und fügt das neues Kind-Node hinzu
+                node.children.pop(child_name) 
                 node.children[new_name] = new_child
                 
                 #print('aft node.children',node.children.keys(), node.positions,'\n')
         
+        # Falls kein Kind-Node, das bearbeitet werden kann
         if node.children == current_node_children :
-            
             for name, child in node.children.items():
                 node.children[name] = merge_node(child)
             return node
         else: 
             return merge_node(node)
+        
     merge_node(trie)
     return trie
 
 
 # Sucht nach Treffer mit Suffix Trie
 def find_hits(node: Node, pattern: str):
-    
+    # Subpattern: Wenn einen Teil des Patterns im Baum gefunden ist, wird der Rest weiter gesucht. Er wird als Subpattern genannt  
+    # Vergleicht die Längen des Subpatterns und des Namen des Nodes  
     def subpattern_is_longer(name_node: str, subpattern: str):
         if len(name_node) >= len(subpattern): return False
         
@@ -66,6 +77,7 @@ def find_hits(node: Node, pattern: str):
         
         return True 
 
+    # Vergleicht den Subpattern und den Namen des Nodes
     def compare_char(name_node: str, subpattern: str):
         if not subpattern_is_longer(name_node,subpattern):
             
@@ -81,10 +93,11 @@ def find_hits(node: Node, pattern: str):
         
         return True
     
+    # Falls der Pattern leer ist
     if not pattern: return node.positions
 
     if not node.is_end_node():
-        for i, name in enumerate(node.children.keys()):
+        for name in node.children.keys():
             if compare_char(name,pattern):
                 #print('true')
                 if subpattern_is_longer(name,pattern):
@@ -92,10 +105,10 @@ def find_hits(node: Node, pattern: str):
                 else:
                     #print('check positions',node.children[name].positions)
                     return node.children[name].positions
-            else: return []
+        return []
             
 import graphviz
-# Erzeugt Digraph und speichert das Bild des Suffix-Tries in PNG-Datei
+# Erzeugt Digraph und speichert das Bild des Suffix-Baumes in PNG-Datei
 def create_digraph(trie: Node, hits_pos:list):
     dot = graphviz.Digraph()
     node_id = 0
@@ -150,33 +163,20 @@ def create_image_base64(png_file_name):
     return(image_64_decode)
 
 # Gibt das Ergebnis zurück
-def get_result(text: str, pattern: str, endchar: str):
+def get_results(text: str, pattern: str, endchar: str):
     trie = build_suffix_trie(text, endchar)
     tree = build_tree(trie)
     hits_pos = find_hits(tree,pattern)
     create_digraph(tree,hits_pos)
+    found = None if len(pattern) == 0 else (True if len(hits_pos) > 0 else False)
     
-    return create_image_base64('suffix_tree.png')
+    return {'image': create_image_base64('suffix_tree.png'),
+            'found': found}
 
 
 # Beispiel
 text = "banana"
 pattern = "an"
 
-get_result(text,pattern,"§")
-# Sucht nach die Positionen der Treffer
-'''
-trie = build_suffix_trie(text, '§')
-tree = build_tree(trie)
-print_trie(tree)
-
-all_hits = find_hits(tree, pattern)
-print("Pattern hits at positions:", all_hits)
-
-# Erzeugt Digraph und Image des Suffix-Tries
-create_digraph(trie,all_hits)
-
-# Erzeugt base64 Image
-base64_image = create_image_base64('suffix_trie.png')
-#print(base64_image)
-'''
+res = get_results(text,pattern,"§")
+#print(res['found'])
