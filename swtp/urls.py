d6819bd5b541104e5072a177334787a929d64929
Nikolai <nikolai.klevtsov@mni.thm.de>
2023-09-01 23:27:57 +0200

    fix homepage

--- swtp/urls.py
+++ swtp/urls.py
@@ -21,7 +21,7 @@ from webapp.views import *
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('horspool/', horspool_view, name='horspool'),
-    path('', base_view, name='base'),
+    path('', homepage_view, name=''),
     path('homepage/', homepage_view, name='homepage'), 
     path('dotplot/', dotplot_view, name='dotplot'),
     path('simple_search/', simple_search_view, name='simple_search'),
